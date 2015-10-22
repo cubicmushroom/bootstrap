3d96e2ba93dd073dd1ce970b23e66c9db76f09f4
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-22 21:45:01 +0100

    Added basic spec tests

--- spec/Console/Command/SetupCommandSpec.php
+++ spec/Console/Command/SetupCommandSpec.php
@@ -5,11 +5,27 @@ namespace spec\CubicMushroom\Tools\ProjectToolbelt\Console\Command;
 use CubicMushroom\Tools\ProjectToolbelt\Console\Command\SetupCommand;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Symfony\Component\Console\Command\Command;
 
+/**
+ * Class SetupCommandSpec
+ *
+ * @package CubicMushroom\Tools\ProjectToolbelt
+ */
 class SetupCommandSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType(SetupCommand::class);
     }
+
+
+    function it_should_be_a_console_command()
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        $this->shouldBeAnInstanceOf(Command::class);
+    }
+
+
+    // See PHPUnit for more tests
 }
--- src/Console/Command/SetupCommand.php
+++ src/Console/Command/SetupCommand.php
@@ -2,6 +2,26 @@
 
 namespace CubicMushroom\Tools\ProjectToolbelt\Console\Command;
 
-class SetupCommand
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+
+/**
+ * Class SetupCommand
+ *
+ * @package CubicMushroom\Tools\ProjectToolbelt
+ *
+ * @see     \spec\CubicMushroom\Tools\ProjectToolbelt\Console\Command\SetupCommandSpec for spec
+ */
+class SetupCommand extends Command
 {
+    /**
+     * Configures the current command.
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('setup')
+            ->setDescription('Sets up the toolbelt for the given project')
+            ->addArgument('path', InputArgument::OPTIONAL, 'Project path');
+    }
 }
