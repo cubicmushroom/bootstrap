6570f56cbd65a40eaa85a7dfefb7580a2ec20f3a
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-23 19:28:48 +0100

    Added APP_ROOT constant for use during tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,13 @@
 <?php
 // This is global bootstrap for autoloading
+
+use Symfony\Component\Filesystem\Filesystem;
+
+$fs = new Filesystem();
+
+$appRoot = __DIR__.'/_output/app_root';
+if (!$fs->exists($appRoot)) {
+    $fs->mkdir($appRoot);
+}
+
+define('APP_ROOT', realpath($appRoot));
